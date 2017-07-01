11dea7812da4b206c42c122a7d40eb94e13f8a08
Ashtell <mhallisawesome@gmail.com>
2017-07-01 14:53:30 -0600

    fixed display error

--- secondary.js
+++ secondary.js
@@ -1,75 +1,78 @@
 // starting gui version now
 /*global swal */
 function makeNewAccount(name) {
-    var newName = name.toLowerCase();                                   //make sure there are no duplicate accounts
-    var accountName = newName + "account";                              // make a new account string
-    var accountNumber = newName + "number";                             // make a new account number
-    var num = Math.random() * (99999) + 1;                              //make a number
-    var Rnum = num.toFixed(0);                                          //set number
-    localStorage.setItem(accountName, 0);                               // make account
+    var newName = name.toLowerCase(); //make sure there are no duplicate accounts
+    var accountName = newName + "account"; // make a new account string
+    var accountNumber = newName + "number"; // make a new account number
+    var num = Math.random() * (99999) + 1; //make a number
+    var Rnum = num.toFixed(0); //set number
+    localStorage.setItem(accountName, 0); // make account
     localStorage.setItem(accountNumber, Rnum);
 
 }
-function createUser(){
+
+function createUser() {
     var name = document.getElementById("makeAccount").innerHTML;
     var Lname = name.toLowerCase();
     var account = Lname + "account";
-    if(localStorage.getItem(account) === null){
+    if (localStorage.getItem(account) === null) {
         makeNewAccount(name);
         swal("Success", "User Account Created", "success");
-    }else{
+    } else {
         swal("Error", "User Account Already Exists", "error");
     }
 }
+
 function deposit() {
-    var number = document.getElementById("number").innerHTML;           //find value of amount deposited
-    var name = document.getElementById("name").innerHTML;               //find value of name
+    var number = document.getElementById("number").innerHTML; //find value of amount deposited
+    var name = document.getElementById("name").innerHTML; //find value of name
     var Lname = name.toLowerCase();
     var account = Lname + "account";
     var accountN = Lname + "number"
-    if (localStorage.getItem(account) === null) {                       //if name isn't found make new account
-        swal('Error', 'User Not Defined', 'error');                     //Shout No!
-    }else{
-    var oldBalance = parseInt(localStorage.getItem(account));
-    var newBalance = parseInt(number) + oldBalance;
-    localStorage.setItem(account, newBalance);
-    document.getElementById("balance").innerHTML = "$" + localStorage.getItem(account);
-    document.getElementById("idn").innerHTML = localStorage.getItem(accountN);
-    }
-}
-
-function withdrawl() {
-    var number = document.getElementById("number").innerHTML;           //find value of amount deposited
-    var name = document.getElementById("name").innerHTML;               //find value of name
-    var Lname = name.toLowerCase();
-    var account = Lname + "account";
-    var accountN = Lname + "number";
-    if (localStorage.getItem(account) === null) {                       //if name isn't found make new account
-        swal('Error', 'User Not Defined', 'error');                     //Shout No!
-    }else{
-    var oldBalance = parseInt(localStorage.getItem(account));
-    var newBalance = oldBalance - parseInt(number);
-    if (newBalance < 0) {
-        document.getElementById("balance").innerHTML = "INSUFF. FUNDS";
+    if (localStorage.getItem(account) === null) { //if name isn't found make new account
+        swal('Error', 'User Not Defined', 'error'); //Shout No!
     } else {
+        var oldBalance = parseInt(localStorage.getItem(account));
+        var newBalance = parseInt(number) + oldBalance;
         localStorage.setItem(account, newBalance);
         document.getElementById("balance").innerHTML = "$" + localStorage.getItem(account);
         document.getElementById("idn").innerHTML = localStorage.getItem(accountN);
     }
 }
-}
 
-function lookUp() {
-    var name = document.getElementById("lookup").innerHTML;             //find value of name
+function withdrawl() {
+    var number = document.getElementById("number").innerHTML; //find value of amount deposited
+    var name = document.getElementById("name").innerHTML; //find value of name
     var Lname = name.toLowerCase();
     var account = Lname + "account";
     var accountN = Lname + "number";
-    if (localStorage.getItem(account) === null) {                       //if name isn't found make new account
-        swal('Error', 'User Not Defined', 'error');                     //Shout No!
+    if (localStorage.getItem(account) === null) { //if name isn't found make new account
+        swal('Error', 'User Not Defined', 'error'); //Shout No!
+    } else {
+        var oldBalance = parseInt(localStorage.getItem(account));
+        var newBalance = oldBalance - parseInt(number);
+        if (newBalance < 0) {
+            document.getElementById("balance").innerHTML = "INSUFF. FUNDS";
+        } else {
+            localStorage.setItem(account, newBalance);
+            document.getElementById("balance").innerHTML = "$" + localStorage.getItem(account);
+            document.getElementById("idn").innerHTML = localStorage.getItem(accountN);
+        }
     }
-    document.getElementById("balance").innerHTML = "$" + localStorage.getItem(account);
-    document.getElementById("idn").innerHTML = localStorage.getItem(accountN);
-    document.getElementById("name").innerHTML = document.getElementById("lookup").innerHTML;
+}
+
+function lookUp() {
+    var name = document.getElementById("lookup").innerHTML; //find value of name
+    var Lname = name.toLowerCase();
+    var account = Lname + "account";
+    var accountN = Lname + "number";
+    if (localStorage.getItem(account) === null) { //if name isn't found make new account
+        swal('Error', 'User Not Defined', 'error'); //Shout No!
+    } else {
+        document.getElementById("balance").innerHTML = "$" + localStorage.getItem(account);
+        document.getElementById("idn").innerHTML = localStorage.getItem(accountN);
+        document.getElementById("name").innerHTML = document.getElementById("lookup").innerHTML;
+    }
 }
 // function displayValue(name, value) {
 //     document.getElementById("balance").innerHTML = value; //display number of dollars
